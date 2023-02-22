72b1981542fbe96334b82e7e1692b4f16404fb5e
CaritaNi <93704532+CaritaNi@users.noreply.github.com>
2023-02-22 18:27:21 +0200

    done

--- components/calculator.js
+++ components/calculator.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Button, ScrollView, Switch, Text, TextInput, TouchableOpacity, View } from 'react-native';
+import { Button, ScrollView, Switch, Text, TextInput, TouchableOpacity, View, Alert } from 'react-native';
 import style from '../styles/style';
 
 const Alcometer = () => {
@@ -16,34 +16,44 @@ const Alcometer = () => {
     { label: 'Female', value: 'female' },
   ];
 
+
   function calculate() {
     const litres = bottles * 0.33;
     const gram = litres * 8 * 4.5;
     const burning = weight / 10;
     const remaining = gram - burning * time;
     let content = null;
-
+  
     if (sex === 'male') {
       content = remaining / (weight * 0.7);
     } else {
       content = remaining / (weight * 0.6);
     }
-
+  
     setResult(content);
 
     if (content > 0) {
       setResult(content);
+      if (content >= 0.5 && content < 0.8) {
+        setResultColor('#E1D24D');
+      } else if (content >= 0.8) {
+        setResultColor('red');
+      } else {
+        setResultColor('green');
+      }
     } else {
       setResult(0);
+      setResultColor('green');
     }
-  }
+    }
+  
 
-  function toggleDarkMode() {
-    setIsDarkMode(!isDarkMode);
-  }
-
+    function toggleDarkMode() {
+      setIsDarkMode(!isDarkMode);
+    }
+    
   return (
-    <ScrollView
+  <ScrollView
     contentContainerStyle={[
       style.container,
       isDarkMode ? style.darkContainer : style.lightContainer,
@@ -76,7 +86,6 @@ const Alcometer = () => {
         />
       </View>
       <View>
-        <TextInput style={style.textInput} />
         <Text style={[style.label, isDarkMode ? style.darkText : style.lightText]}>Time :</Text>
         <TextInput
           style={style.textInput}
@@ -86,27 +95,31 @@ const Alcometer = () => {
         />
       </View>
 
-      <View style={style.genderContainer}>
-        <Text style={[style.label, isDarkMode ? style.darkText : style.lightText]}>Gender</Text>
-          {gender.map((option) => (
-        <TouchableOpacity 
-           key={option.value}
-          onPress={() => setSex(option.value)}
-        >
-        <View style={[style.radioButtonIcon, isDarkMode ? style.radioButtonIconDark : style.radioButtonIcon]} />
-          <Text style={[style.radioButtonText, style.radioButtonIconChecked , isDarkMode ? style.darkText : style.lightText]}>{option.label}</Text>
-        </TouchableOpacity>
+      <View>
+        <Text style={[style.label, isDarkMode ? style.darkText : style.lightText]}>Gender: </Text>
+        {gender.map((option) => (
+          <TouchableOpacity 
+            key={option.value}
+            onPress={() => setSex(option.value)}
+          >
+          <View style={[style.radioButtonIcon, isDarkMode ? style.radioButtonIconDark : style.radioButtonIcon,
+              sex === option.value && style.radioButtonIconChecked
+            ]} />
+          <Text style={[style.radioButtonText, isDarkMode ? style.darkText : style.lightText]}>{option.label}</Text>
+          </TouchableOpacity>
         ))}
       </View>
 
-        <View style={style.button}>
-          <TouchableOpacity onPress={() => calculate()}>
-            <Text style={style.buttonText}>SUBMIT ♥</Text>
-          </TouchableOpacity>
-        </View>
-        <Text style={[style.label, isDarkMode ? style.darkText : style.lightText]}>Result : {result.toFixed(2)}</Text>
-    </ScrollView>
+      <View style={style.button}>
+        <TouchableOpacity onPress={() => calculate()}>
+          <Text style={style.buttonText}>SUBMIT ♥</Text>
+        </TouchableOpacity>
+      </View>
+        <Text style={[style.label, { color: resultColor }]}>
+          Result : {result.toFixed(2)}
+        </Text>
+  </ScrollView>
   );
-};
+  };
 
 export default Alcometer;
--- styles/style.js
+++ styles/style.js
@@ -3,17 +3,15 @@ import { StyleSheet } from 'react-native';
 const styles = StyleSheet.create({
   lightContainer: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#F0FFD6',
     alignItems: 'center',
     justifyContent: 'center',
-    paddingHorizontal: 20,
   },
   darkContainer: {
     flex: 1,
     backgroundColor: '#181515',
     alignItems: 'center',
     justifyContent: 'center',
-    paddingHorizontal: 20,
   },
   darkText:{
     color: "#fff"
@@ -26,14 +24,15 @@ const styles = StyleSheet.create({
   label: {
     fontSize: 20,
     fontWeight: 'bold',
+
     marginVertical: 10,
   },
   textInput: {
-    backgroundColor: '#f2f2f2',
+    backgroundColor: '#fff',
     padding: 10,
     borderRadius: 5,
     marginBottom: 10,
-    width: '100%',
+    width: 100,
   },
   genderButton: {
     backgroundColor: '#239F9F',
@@ -41,16 +40,13 @@ const styles = StyleSheet.create({
     padding: 10,
     margin: 10,
     width: '80%',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
-
   button: {
     backgroundColor: '#4CAF50',
     borderRadius: 4,
     padding: 10,
     margin: 10,
-    width: '50%',
+    width: '30%',
     alignItems: 'center',
     justifyContent: 'center',
   },
@@ -60,7 +56,7 @@ const styles = StyleSheet.create({
     fontWeight: 'bold',
   },
   text: {
-    color: '#fff',
+    color: '#4CAF50',
     fontSize: 18,
     fontWeight: 'bold',
   },
@@ -86,13 +82,13 @@ const styles = StyleSheet.create({
     marginHorizontal: 10,
   },
   radioButtonIconChecked: {
-    backgroundColor: '4CAF50',
+    backgroundColor: '#4CAF50',
     borderColor: '#3f51b5',
   },
   radioButtonText: {
     fontSize: 16,
     fontWeight: 'bold',
-    color: '#000',
+    color: '#black',
   },
 });
 
